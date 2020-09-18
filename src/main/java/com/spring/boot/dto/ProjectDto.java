9b00b99547f6e1d0a84401a15982d3c9c4ddcb52
ajith0110 <santhan.ajith50@gmail.com>
2020-09-18 11:29:50 +0530

    first commit

---
+++ src/main/java/com/spring/boot/dto/ProjectDto.java
@@ -0,0 +1,32 @@
+package com.spring.boot.dto;
+
+public class ProjectDto {
+	private Long id;
+	private String name;
+	private String abbreviation;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+
+}
