4aadb7214fb26ea7a7690d5e926b8efbbf85d848
dishanth <dishanth6@gmail.com>
2020-09-22 21:53:36 +0530

    Module update done

--- src/main/java/com/spring/boot/controllers/ModuleController.java
+++ src/main/java/com/spring/boot/controllers/ModuleController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,8 +34,14 @@ public class ModuleController {
 	@DeleteMapping(value="module/delete/{id}")
 	public String deletemodule(@PathVariable Long id) {
 		moduleService.deletemodule(id);
-		return "Deleted";
+		return "Deleted";		
 		
 	}
 
+	@PutMapping(value = "/module/update")
+	public ResponseEntity<Object> updateModule(@RequestBody ModuleNew module){
+		moduleService.addModule(module);
+		return new ResponseEntity<Object>("Updated", HttpStatus.OK);
+	}
+	
 }
